6dfd3858eb7be9c9765337dab5f7c9b56c4724e6
banuka <banukaramanayaka76@gmail.com>
2023-04-08 20:59:15 +0530

    fdsd

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className=' mx-auto h-fit mt-5 shadow-lg' >
+        <img src="./images/airbnb logo.png" alt="" className='p-2 h-12'/>
+    </div>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <div className='h-screen '>
+        <img src="./images/Group 77.png" alt="" className='mx-auto mt-8' />
+        </div>
+        
+        <div className='ml-5 w-[80%]'>
+        <h4 className='text-5xl font-bold '>Online Experience</h4>
+        <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vitae dolores nostrum odio consequuntur, laborum iste quidem accusantium esse, officiis ab quibusdam reiciendis quae rerum tempore explicabo, voluptas fugiat debitis!</p>
+        </div>
+        
+    </div>
+  )
+}
+
